424002dd07584406f3c11183a22f4df3526266de
Jack Spence <jack00spence@gmail.com>
2025-02-02 23:04:11 -0500

    Created Navbar and Footer

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css';
+
+function Navbar () {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Images</li>
+        <li>The App</li>
+        <li>Buisness Solutions</li>
+        <li>About Us</li>
+      </ul>
+    </nav>
+  );
+
+}
+
+export default Navbar;
